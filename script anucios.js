82a27dadcb247df055abdd26942ae0eb3896b302
FilipeISTEC <116121180+FilipeISTEC@users.noreply.github.com>
2023-09-28 11:37:00 +0100

    Implementação de fluidez
    Adicionas as fotos para cada uma das paginas e scrpit para interacao de pesquisa

---
+++ script anucios.js
@@ -0,0 +1,74 @@
+// script.js
+
+
+// Obtenha o botão "Read more" por ID
+var readMoreButton = document.getElementById("readMoreButton1");
+
+// Adicione um ouvinte de evento de clique ao botão
+readMoreButton.addEventListener("click", function() {
+    // Redirecione o usuário para a URL desejada
+    window.open("https://www.portalviva.pt/pt/homepage/onde-carregar.aspx", "_blank");
+});
+
+
+
+
+// Obtenha o botão "Read more" por ID
+var readMoreButton = document.getElementById("readMoreButton2");
+
+// Adicione um ouvinte de evento de clique ao botão
+readMoreButton.addEventListener("click", function() {
+    // Redirecione o usuário para a URL desejada
+    window.open("https://www.dn.pt/sociedade/circulacao-na-linha-amarela-do-metro-de-lisboa-interrompida-por-avaria-em-comboio-17069906.html", "_blank");
+});
+
+
+
+// script.js
+
+// Obtenha o botão "Read more" por ID
+var readMoreButton = document.getElementById("readMoreButton3");
+
+// Adicione um ouvinte de evento de clique ao botão
+readMoreButton.addEventListener("click", function() {
+    // Redirecione o usuário para a URL desejada
+    window.open("https://www.dn.pt/sociedade/atropelamento-ferroviario-mortal-corta-linha-do-norte-na-povoa-de-santa-iria-16871361.html","_blank");
+});
+
+
+
+// script.js
+
+// Obtenha o botão "Read more" por ID
+var readMoreButton = document.getElementById("readMoreButton4");
+
+// Adicione um ouvinte de evento de clique ao botão
+readMoreButton.addEventListener("click", function() {
+    // Redirecione o usuário para a URL desejada
+    window.open("https://www.noticiasaominuto.com/economia/2408604/comboio-historico-do-douro-tornou-se-rentavel-pela-primeira-vez-em-2023", "_blank");
+});
+
+// script.js
+
+// Obtenha o botão "Read more" por ID
+var readMoreButton = document.getElementById("readMoreButton5");
+
+// Adicione um ouvinte de evento de clique ao botão
+readMoreButton.addEventListener("click", function() {
+    // Redirecione o usuário para a URL desejada
+    window.open("https://www.dn.pt/sociedade/passe-ferroviario-nacional-para-comboios-regionais-entra-esta-terca-feira-em-vigor-16787417.html", "_blank");
+});
+
+
+// script.js
+
+// Obtenha o botão "Read more" por ID
+var readMoreButton = document.getElementById("readMoreButton6");
+
+// Adicione um ouvinte de evento de clique ao botão
+readMoreButton.addEventListener("click", function() {
+    // Redirecione o usuário para a URL desejada
+    window.open("https://www.dn.pt/internacional/autocarro-cai-num-precipicio-e-provoca-quatro-mortos-e-50-feridos-na-guatemala-16148865.html", "_blank");
+});
+
+
